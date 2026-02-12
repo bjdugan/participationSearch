6c754ebb723f081bbeca1dc65f0db30716a994e4
bjdugan <ardentArmadillo24@proton.me>
2026-02-12 17:33:49 -0500

    Updated IPEDS OBEREG labels to match current standards.

--- data-raw/participation.R
+++ data-raw/participation.R
@@ -5,6 +5,7 @@ library(odbc)
 library(readxl)
 library(DBI)
 library(dplyr)
+library(tidyr)
 library(purrr)
 
 # NSSE particpation
@@ -12,6 +13,8 @@ participation <- read_xlsx("C:/nsse/2025/nsse kiosk (2013-25).xlsx") |>
   # drop test schools and special cases, and Canadians/foreign institutions for now
   filter(unitid != 888888 & SPLOp == 0 & unitid < 900000) |>
   select(unitid, admin_year, BCSSE, FSSE, module1label, module2label, cons_full)
+
+# Canadian and other foreign institutions might have lat/lon based on cities in the maps or rnaturalearth or tidygeocoder packages.
 
 
 # for reference, omit
@@ -65,12 +68,25 @@ ipeds_labels <- map(
     select(varName, Codevalue, valueLabel) |>
     collect() |>
     mutate(Codevalue = as.integer(Codevalue))
+
 ) |>
   set_names(
     paste0("HD20", substr(unique(participation$admin_year) - 3, 3, 4))
   )
 
-# for replacing IPEDS codes with labels
+# region labels were [Name] [list of state abb] through 2018; coerce to modern format [Name] [Comma separated list]
+ipeds_labels <- map_at(
+  ipeds_labels,
+  paste0("HD20", 10:18),
+  \(x) left_join(x,
+                 filter(ipeds_labels[[1]], varName == "OBEREG"),
+                 by = c("varName", "Codevalue")) |>
+    mutate(valueLabel.x = if_else(!is.na(valueLabel.y), valueLabel.y,
+                                  valueLabel.x)) |>
+    select(varName, Codevalue, valueLabel = valueLabel.x)
+)
+
+# for replacing IPEDS codes with interpretable labels
 apply_ipeds_labels <- function(x, y, var) {
   by_var <- setNames(c("Codevalue"), var)
 
@@ -83,9 +99,9 @@ apply_ipeds_labels <- function(x, y, var) {
 ipeds <- map2(ipeds, ipeds_labels, apply_ipeds_labels, var = "OBEREG") |>
   map2(ipeds_labels, apply_ipeds_labels, var = "CONTROL")
 
-
 # match NSSE participation, minor adjustments
-participation <- left_join(participation, bind_rows(ipeds), by = c("unitid" = "UNITID", "admin_year"))
+participation <- left_join(participation, bind_rows(ipeds),
+                           by = c("unitid" = "UNITID", "admin_year"))
 
 # quality check: a very small handful are missing some data, possibly from having UNITID reassigned...
 #count(participation, is.na(CONTROL) | is.na(OBEREG) | is.na(INSTNM))
@@ -93,6 +109,12 @@ participation <- left_join(participation, bind_rows(ipeds), by = c("unitid" = "U
 participation <- filter(participation, !is.na(LATITUDE) & !is.na(LONGITUD))
 
 # any particular adjustments for display or download
+participation <- mutate(participation,
+                        module1label = replace_na(module1label, ""),
+                        module2label = replace_na(module2label, ""),
+                        cons_full = replace_na(cons_full, "")
+)
+
 
 # save to /data (VERY small)
 usethis::use_data(participation, overwrite = TRUE)
